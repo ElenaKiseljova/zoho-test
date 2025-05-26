f237559a9ffe7f48b91e9c35140721f1c4371d0b
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-05-26 21:25:09 +0300

    add: create Deal functionality

--- app/Http/Controllers/Zoho/DealController.php
+++ app/Http/Controllers/Zoho/DealController.php
@@ -7,59 +7,82 @@ use Illuminate\Http\Request;
 
 class DealController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
+  public $DealURL = 'https://crm.zoho.eu/crm/v2.2/Deals';
+
+  /**
+   * Show the form for creating a new resource.
+   */
+  public function create()
+  {
+    //
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   */
+  public function store(Request $request)
+  {
+    // Response data
+    $respData = [
+      'error' => null,
+      'success' => null,
+      'message' => null,
+      'data' => null
+    ];
+
+    // Init cURL
+    $ch = curl_init();
+
+    // Settings cURL
+    curl_setopt_array($ch, $this->getCUrlOptions($this->DealURL, $request->all()));
+
+    // Execute
+    $response = curl_exec($ch);
+
+    // JSON to array
+    $response = json_decode($response, 1);
+
+    dd($response, $request->all());
+
+    if (isset($response['status']) && $response['status'] === 'error') {
+      $respData['error'] = [
+        'code' => $response['code'],
+        'message' => $response['message'],
+      ];
+    } else if (isset($response['data'])) {
+      $respData['success'] = true;
+      $respData['message'] = 'Deal were successfully created';
+      $respData['data'] = $response['data'][0];
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+    return $respData;
+  }
+
+  /**
+   * Get array of options for cUrl
+   */
+  private function getCUrlOptions(string $url, ?array $postData = null): array
+  {
+    // Auth
+    $AuthenticationController = new AuthenticationController;
+
+    // Base
+    $options = [
+      CURLOPT_URL => $url,
+      CURLOPT_RETURNTRANSFER => 1,
+      CURLOPT_SSL_VERIFYPEER => 0,
+      CURLOPT_HTTPHEADER => [
+        'Content-Type: application/x-www-form-urlencoded',
+        'Authorization: Zoho-oauthtoken ' . ($AuthenticationController->accessToken() ?? ''),
+      ],
+    ];
+
+    // Post
+    if ($postData) {
+      $options[CURLOPT_POST] = 1;
+      $options[CURLOPT_POSTFIELDS] = json_encode($postData);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
-    }
+    return $options;
+  }
 }
