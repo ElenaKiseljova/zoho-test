56b0826102865661b8bdb7e7eb5286403fb7e218
Elena Kiseljova <e.a.kiseljova@gmail.com>
2025-05-26 21:21:42 +0300

    upd: display stages (without styles)

--- resources/js/Components/Form.vue
+++ resources/js/Components/Form.vue
@@ -1,7 +1,24 @@
-<script setup></script>
+<script setup>
+defineProps({
+  stages: {
+    type: [Array, null],
+    required: false,
+  },
+});
+</script>
 
 <template>
-    <div class="py-2 text-red-500">Form</div>
+  <div class="py-2">
+    <template v-if="stages?.length">
+      <h2 class="text-red-500 font-bold mb-2">Stages!!!</h2>
+
+      <ul class="flex-col space-y-1 text-orange-400">
+        <li v-for="stage in stages" :key="stage.id">
+          {{ stage.name }}
+        </li>
+      </ul>
+    </template>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
